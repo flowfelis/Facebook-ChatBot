73109de8d34d0d00c8288a51642d84e46eeb63ec
Alican <alicandonmez90@gmail.com>
2017-11-17 23:03:57 +0100

    Basics

--- main.py
+++ main.py
@@ -2,22 +2,19 @@ import os
 import requests
 from flask import Flask, request
 
-token = os.environ.get('PAGE_ACCESS_TOKEN')
+page_token = os.environ.get('PAGE_ACCESS_TOKEN')
+verify_token = os.environ.get('VERIFY_TOKEN')
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
-def verify():
-
-    if request.args.get('hub.challenge'):
-        return request.args.get('hub.challenge')
-    else:
-        return "<h1>Welcome to ChatBot App.</h1> <h4>Please go to Facebook and find " \
-        "Mr. Chat Bot for a fun chit-chat :)</h4>"
-
-
 @app.route('/', methods=['GET', 'POST'])
-def receive():
+def index():
+    if request.method == 'GET':
+        if request.args.get('hub.verify_token') == verify_token:
+            return request.args.get('hub.challenge')
+        return "Wrong Verify Token"
+        # return "<h1>Welcome to ChatBot App.</h1> <h4>Please go to Facebook and find " \
+        #     "Mr. Chat Bot for a fun chit-chat :)</h4><hr><quote>Developed By Flowfelis</quote>"
     if request.method == 'POST':
         data = request.get_json()
         sender_id = data['entry'][0]['messaging'][0]['sender']['id']
@@ -25,7 +22,8 @@ def receive():
         send_text = "Hi There!"
         payload = {'recipient': {'id': sender_id}, 'message': {'text': send_text}}
 
-        r = requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + token, json=payload)
+        r = requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + page_token, json=payload)
+        pass
 
 if __name__ == '__main__':
     app.run(debug=True)
