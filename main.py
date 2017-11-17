dbad114c2dfea13661c01db3d89597c534565629
Alican <alicandonmez90@gmail.com>
2017-11-17 20:14:05 +0100

    debug

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import json
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -15,10 +16,17 @@ def verify():
 
 @app.route('/', methods=['GET', 'POST'])
 def receive():
-    data = request.get_json() # cannot return this
-    app.logger.debug(data)
-
-    return 'data'
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.data.decode())
+            text = data['entry'][0]['messaging'][0]['message']['text']
+            sender = data['entry'][0]['messaging'][0]['sender']['id']
+            payload = {'recipient': {'id': sender}, 'message': {'text': "Hello World"}}
+            return "<p>text: {0}</p><p>sender: {1}</p>".format(text, sender)
+            # r = requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + token, json=payload)
+        except Exception as e:
+            print(traceback.format_exc())
+    elif request.method == 'GET':
 
 if __name__ == '__main__':
     app.run(debug=True)
