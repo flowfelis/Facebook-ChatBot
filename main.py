f5a41f8918f6781160c2181dc2fbad7f312c1f23
Alican <alicandonmez90@gmail.com>
2017-11-17 03:14:58 +0100

    webhook validation

--- main.py
+++ main.py
@@ -7,14 +7,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def verify():
-    # when the endpoint is registered as a webhook, it must echo back
-    # the 'hub.challenge' value it receives in the query arguments
-    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
-        if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
-            return "Verification token mismatch", 403
-        return request.args["hub.challenge"], 200
-
-    return "Hello world", 200
+    if request.args.get('hub.challenge'):
+        return request.args.get('hub.challenge')
+    else:
+        return "Hello World!"
 
 if __name__ == '__main__':
     app.run(debug=True)
