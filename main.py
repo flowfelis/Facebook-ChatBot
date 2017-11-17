3af908ba7b774c541e40eeb8114c08609b12fce4
Alican <alicandonmez90@gmail.com>
2017-11-18 00:06:34 +0100

    Skeleton Ready

--- main.py
+++ main.py
@@ -17,8 +17,6 @@ def index():
         return "<h1>Welcome to ChatBot App.</h1> <h4>Please go to Facebook and find " \
         "Mr. Chat Bot for a fun chit-chat :)</h4><quote>Developed By Flowfelis</quote>"
 
-
-
 @app.route('/', methods=['POST'])
 def receive():
     data = request.get_json()
