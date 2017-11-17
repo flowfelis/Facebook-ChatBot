e31b870ff57613e9a27829e737594861ae757679
Alican <alicandonmez90@gmail.com>
2017-11-17 03:56:16 +0100

    get msg

--- main.py
+++ main.py
@@ -10,7 +10,14 @@ def verify():
     if request.args.get('hub.challenge'):
         return request.args.get('hub.challenge')
     else:
-        return "Hello World!"
+        return "<h1>Welcome to ChatBot App.</h1> <h4>Please go to Facebook and find " \
+        "Mr. Chat Bot for a fun chit-chat :)</h4>"
+
+@app.route('/', methods=['POST'])
+def receive():
+    data = request.get_json()
+    return data
+
 
 if __name__ == '__main__':
     app.run(debug=True)
