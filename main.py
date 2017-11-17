217d7b8320d888bb31688497f507e5769b9947e3
Alican <alicandonmez90@gmail.com>
2017-11-17 02:45:31 +0100

    change of token var

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    sha = request.args.get("sha1")
-    return sha
+    return 'hello world!'
 
 if __name__ == '__main__':
     app.run(debug=True)
