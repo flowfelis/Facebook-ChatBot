6e5aabb6b18c0e0ed6dfc170b55b6b3101ce0ece
Alican <alicandonmez90@gmail.com>
2017-11-21 22:06:39 +0100

    can retreive tone json

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import os
+import json
 import requests
 import traceback
 from flask import Flask, request, redirect
 from watson_developer_cloud import ToneAnalyzerV3
 
+# tone_analyzer = ToneAnalyzerV3(
+#     username = os.environ.get('TONE_USERNAME'),
+#     password = os.environ.get('TONE_PASSWORD'),
+#     version = os.environ.get('TONE_VERSION'))
+
 tone_analyzer = ToneAnalyzerV3(
-    username='YOUR SERVICE USERNAME',
-    password='YOUR SERVICE PASSWORD',
-    version='2017-09-26')
+    username = 'bf4d9c4a-a8d3-4599-97b5-63ea77b665d6',
+    password = 'oe8mEef2JXQv',
+    version = '2017-09-26')
 
 page_token = os.environ.get('PAGE_ACCESS_TOKEN')
 verify_token = os.environ.get('VERIFY_TOKEN')
@@ -27,7 +33,12 @@ def receive():
     data = request.get_json()
     sender_id = data['entry'][0]['messaging'][0]['sender']['id']
     receive_text = data['entry'][0]['messaging'][0]['message']['text']
+
+    print(json.dumps(tone_analyzer.tone(tone_input=receive_text,
+        content_type="text/plain"), indent=2))
+
     send_text = "Hi There!"
+
     payload = {'recipient': {'id': sender_id}, 'message': {'text': send_text}}
 
     r = requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + page_token, json=payload)
